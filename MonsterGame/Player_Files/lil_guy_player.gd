e6ca9ae60613776a61ac76bd9b317bff73f2228b
MichaelCapriel <caprielx@gmail.com>
2023-09-27 12:06:33 -0500

    Update 4
    Animations are "working" except for starting jump cycle it refuses to pull and does not want to play no matter what I do.
    
    current code would have to be completely changed to revert more like the ver that John had for us. My branch is officially the broken test branch

--- MonsterGame/Player_Files/lil_guy_player.gd
+++ MonsterGame/Player_Files/lil_guy_player.gd
@@ -1,28 +1,64 @@
 extends CharacterBody2D
 
+@export var speed: float = 300.0
+@export var jump_velocity: float = -400.0
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var animation_locked: bool = false
+var direction: Vector2 = Vector2.ZERO
+var was_in_air: bool = false
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		was_in_air = true
+	if was_in_air == true:
+		land()
+		was_in_air = false
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		jump()
+	
+	direction = Input.get_vector("left", "right", "up", "down")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction.x * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = move_toward(velocity.x, 0, speed)
+	
 	move_and_slide()
+	update_animation()
+	update_facing_direction()
+	
+func update_animation():
+	if not animation_locked:
+		if not is_on_floor():
+			animated_sprite.play(("Jump_Start_Loop"))
+		else:
+			if direction.x != 0:
+				animated_sprite.play("Walking")
+			else:
+				animated_sprite.play("Idle")
+		
+func update_facing_direction():
+	if direction.x > 0:
+		animated_sprite.flip_h = false
+	elif direction.x < 0:
+		animated_sprite.flip_h = true
+		
+func jump():
+	velocity.y = jump_velocity
+	animated_sprite.play("Jump_Start")
+	animation_locked = true
+
+func _on_animated_sprite_2d_animation_finished():
+	if(animated_sprite.animation == "Fall"):
+		animation_locked = false
+	elif(animated_sprite.animation == "Jump_Start"):
+		animation_locked = false
+
+func land():
+	animated_sprite.play("Fall")
+	animation_locked = true
+
