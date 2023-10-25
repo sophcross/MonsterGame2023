9249f049ff66ec0448d4da82bb82884046d20292
MichaelCapriel <caprielx@gmail.com>
2023-10-25 07:50:17 -0500

    Update: Enemies
    Working on Ray casting slow going

--- MonsterGame/Enemy/knight.gd
+++ MonsterGame/Enemy/knight.gd
@@ -14,14 +14,15 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		#if $Raycast.is_colliding() && is_on_floor():
-			#flip()
+		if $RayCast2D.is_colliding() && is_on_floor():
+			flip()
+	
 	velocity.x = SPEED
 	move_and_slide()
 
 func flip():
 	facing_right = !facing_right
-	scale.x = abs(scale.x) * -1
+	scale.x = abs(scale.x) * 1
 	if facing_right:
 		SPEED = abs(SPEED)
 	else:
